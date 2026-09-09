b9fc3a64c778393cf8c27588ef2470594cf57787
garvit840 <YOUR_GARVIT840_VERIFIED_EMAIL>
2026-09-09 22:57:18 +0530

    day17[i]:program to check if a number is an Armstrong number

---
+++ day17[i].c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num > 0) {
+        digit = num % 10;              // get last digit
+        sum = sum + digit * digit * digit;
+        num = num / 10;                // remove last digit
+    }
+
+    if (sum == original) {
+        printf("Armstrong number");
+    } else {
+        printf("Not an Armstrong number");
+    }
+
+    return 0;
+}
