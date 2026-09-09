f157aff0d6f5e26e82e9b35f82b55c1007d2bda1
garvit840 <YOUR_GARVIT840_VERIFIED_EMAIL>
2026-09-09 23:17:19 +0530

    day17[ii:program to check if a number is prime

---
+++ day17[ii].c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num,i,count=0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    for(int i=1;i<=num;i++)
+    {
+        if(num%i==0){
+            count++;
+        }
+    }    
+    if(count==2){
+        printf("prime number");
+    }        
+    else{
+        printf("not a prime number");
+    }    
+
+return 0;
+}
