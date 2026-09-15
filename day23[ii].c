ffd7ff16610dbf6a4958a33a9587fbcddbec9e02
garvit840 <YOUR_GARVIT840_VERIFIED_EMAIL>
2026-09-16 01:37:49 +0530

    day23[ii]:Write a program to print the following pattern: ***** ***** ***** ***** *****

---
+++ day23[ii].c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{//Write a program to print the following pattern: ***** ***** ***** ***** *****   
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;i<=5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+return 0;
+
+
+}
