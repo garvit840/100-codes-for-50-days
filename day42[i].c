5089fd110cd9481da757e5549155e9864b1abf7e
garvit840 <YOUR_GARVIT840_VERIFIED_EMAIL>
2026-09-20 23:56:57 +0530

    add

---
+++ day42[i].c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{//Count vowels and consonants in a string
+    char str[100];
+    int i, vowel = 0, consonant = 0;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+           str[i]=='o' || str[i]=='u' || str[i]=='A' ||
+           str[i]=='E' || str[i]=='I' || str[i]=='O' ||
+           str[i]=='U')
+        {
+            vowel++;
+        }
+        else if((str[i]>='a' && str[i]<='z') ||
+                (str[i]>='A' && str[i]<='Z'))
+        {
+            consonant++;
+        }
+    }
+
+    printf("Vowels = %d\n", vowel);
+    printf("Consonants = %d\n", consonant);
+
+    return 0;
+}
---
+++ day42[ii].c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{//Convert lowercase string to uppercase without built-in functions
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("Uppercase string: %s", str);
+
+    return 0;
+}
