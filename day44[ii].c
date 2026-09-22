caaa809e5da6046e2d84dc86fd11548cf085a1f9
garvit840 <YOUR_GARVIT840_VERIFIED_EMAIL>
2026-09-22 23:28:25 +0530

    add

---
+++ day44[ii].c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{//Replace spaces with hyphens in a string
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            str[i] = '-';
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
